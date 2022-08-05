c20cd55e05fadbae75ed37369c53927d9e0d1376
vvvonly <36775017+vvvonly@users.noreply.github.com>
2022-08-05 22:31:57 +0000

    commit

--- js/product-list.js
+++ js/product-list.js
@@ -20,7 +20,7 @@ class ProductList {
           <div class="card-body">
             <h5 class="card-title">${product.title}</h5>
             <p class="card-text">${product.description}</p>
-            <a href="#" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#prodact-info-modal" data-id=${product.id}>Info</a>
+            <a href="#" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#product-info-modal" data-id=${product.id}>Info</a>
             <a href="#" class="btn btn-primary btn-buy" data-id=${product.id}>${product.price}Buy</a>
           </div>
         </article>`;
